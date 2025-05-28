092326ff3b700c671666dab44a528e5498bdf0e1
Sujan Banjara <banjara.suzan12@gmail.com>
2025-05-28 13:07:14 -0500

    Added Order class to manage list of OrderItem objects with total price calculation

--- DELI-cious/src/main/java/models/Order.java
+++ DELI-cious/src/main/java/models/Order.java
@@ -4,41 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-    private List<String > sandwiches = new ArrayList<>();
-    private List<String> drinks = new ArrayList<>();
-    private List<String> chips = new ArrayList<>();
+    private List<OrderItem> items = new ArrayList<>();
 
-    public void addSandwich(String sandwich) {
-        sandwiches.add(0, sandwich);
+    public void addItem(OrderItem item) {
+        items.add(0, item);
     }
 
-    public void addDrink(String drink) {
-        drinks.add(0, drink);
-    }
-
-    public void addChips(String chip) {
-        chips.add(0, chip);
-    }
-
-    public List<String> getSandwiches() {
-        return sandwiches;
-    }
-
-    public List<String> getDrinks() {
-        return drinks;
-    }
-
-    public List<String> getChips() {
-        return chips;
+    public List<OrderItem> getItems() {
+        return items;
     }
 
     public void clear() {
-        sandwiches.clear();
-        drinks.clear();
-        chips.clear();
+        items.clear();
     }
 
     public boolean isEmpty() {
-        return sandwiches.isEmpty() && drinks.isEmpty() && chips.isEmpty();
+        return items.isEmpty();
     }
+
+    public double getTotalPrice() {
+        return items.stream()
+                .mapToDouble(OrderItem::getPrice).sum();
+    }
+
 }
