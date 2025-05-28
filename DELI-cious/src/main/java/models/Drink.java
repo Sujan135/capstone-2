21bbcefd41d93c504ba2d2ccc274970056b96381
Sujan Banjara <banjara.suzan12@gmail.com>
2025-05-28 14:02:12 -0500

    Added screen to get drink size and flavor from user and create a Drink

--- DELI-cious/src/main/java/models/Drink.java
+++ DELI-cious/src/main/java/models/Drink.java
@@ -2,11 +2,17 @@ package models;
 
 public class Drink extends Product{
     private String size;
+    private String flavor;
+    private double price;
 
-    public Drink(String size) {
+    public Drink(String size, String flavor) {
         this.size = size;
-        this.name = size + "Drink";
-        this.price = switch (size) {
+        this.flavor = flavor;
+        this.name = size + " " + flavor + " Drink";
+        this.price = calculatePrice(this.size);
+    }
+    private double calculatePrice(String size) {
+        return switch (size) {
             case "Small" -> 2.00;
             case "Medium" -> 2.50;
             case "Large" -> 3.00;
@@ -16,5 +22,10 @@ public class Drink extends Product{
     @Override
     public String getDescription() {
         return name;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
     }
 }
